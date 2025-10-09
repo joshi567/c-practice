ac9809df753e4985d3e57dff1da9d404ef3fe611
joshi567 <gjoshitha567@gmail.com>
2025-10-09 20:53:15 +0530

    Add files via upload

---
+++ min_sec.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Time taken to finish workout in min is:")
+    ;scanf("%d",&a);
+    printf("Time Taken to complete workout in sec is:%d\n",a*60);
+}
